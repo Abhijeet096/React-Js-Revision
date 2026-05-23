5dc1447653b1342c55297794d9b818d6818596fa
Abhijit <tradeabhiyt@gmail.com>
2026-05-23 20:39:22 +0530

    Commit form-handling folder to github

---
+++ form-handling/src/App.jsx
@@ -0,0 +1,15 @@
+// import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <form action="">
+        <input type="text" placeholder='Enter your name' />
+        {/* <input type="text" /> */}
+        <button>Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
